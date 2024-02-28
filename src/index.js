362bc24ff2646f378af9db5d303c0429112258af
Michelle Li <rphmichelle@gmail.com>
2024-02-28 02:00:25 -0500

    edit color scheme

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", async () => {
     const mountain2 = document.getElementById('mountain2')
     const mountain3 = document.getElementById('mountain3')
     const mountain4 = document.getElementById('mountain4')
+    const cloud1 = document.getElementById('cloud1')
+    const cloud2 = document.getElementById('cloud2')
     const bird = document.getElementById('bird')
 
     window.addEventListener('scroll', ()=> {
@@ -13,7 +15,9 @@ document.addEventListener("DOMContentLoaded", async () => {
         mountain2.style.left = value * -.7 + 'px'
         mountain3.style.left = value * 1 + 'px'
         mountain4.style.left = value * .8 + 'px'
-        bird.style.left = value * 1 + 'px'
+        bird.style.left = value * .5 + 'px'
+        cloud2.style.left = value * -1 + 'px'
+        cloud1.style.left = value * 1 + 'px'
     })
 })
 
