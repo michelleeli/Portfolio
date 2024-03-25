f066accff7e96f53032b5ce9933aabc4b546361c
Michelle Li <rphmichelle@gmail.com>
2024-03-25 16:32:14 -0400

    change language icons

--- src/index.js
+++ src/index.js
@@ -27,6 +27,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     })
 
     function updateSkill(e) {
+        about.style.backgroundColor = '#D694A3'
         about.innerText = ""
         const img = document.createElement("img");
         img.src = "https://skillicons.dev/icons?i=git,python,ruby,react,js,redux,express,nodejs,mongodb,css,postgres,mysql,html&perline=4"
@@ -34,10 +35,13 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
 
     function updateInfo(e) {
+        about.style.backgroundColor = '#e298a9'
+
         about.innerText = "Personal growth has always been the main driving force behind many of the decisions I make - including the decision to change careers from being a pharmacist to becoming a software engineer. I am grateful for the skills of critical thinking, problem solving, collaborative teamwork, and leadership that I have honed as a medical professional and am now able to apply to my work as a software engineer. It makes me excited to grow alongside the tech industry, work with others who share the same passions, and learn from those who have different interests than my own!"
     }
 
     function updateFacts(e) {
+        about.style.backgroundColor = '#B27B94'
         about.innerText = 'I love discovering new places and have traveled to more than 20 countries, I grew up taking swimming and latin ballroom dance lessons. I was a cheerleader and dance team director in my academic years and now my favorite ways to stay active include going to the gym and taking pilates and zumba classes, My favorite genres are fiction novels and memoirs and my favorite podcasts are neuroscience and self help, I play the piano and spent my quarantine days learning how to play the ukelele, My favorite creative outlets are bullet journaling and creating lifestyle content'
     }
     skills.addEventListener('click', updateSkill)
